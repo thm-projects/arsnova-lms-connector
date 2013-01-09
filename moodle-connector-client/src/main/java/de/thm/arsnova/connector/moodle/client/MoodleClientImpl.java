a25add4fdfdfcd709ed40f91ed3cdf16b0097518
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2013-01-09 11:18:09 +0100

    Added HTTP basic auth to client library
    Client needs to be configured by using setters or by creating spring bean
    using property configuration.

--- moodle-connector-client/src/main/java/de/thm/arsnova/connector/moodle/client/MoodleClientImpl.java
+++ moodle-connector-client/src/main/java/de/thm/arsnova/connector/moodle/client/MoodleClientImpl.java
@@ -1,6 +1,11 @@
 package de.thm.arsnova.connector.moodle.client;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import de.thm.arsnova.connector.moodle.model.Courses;
@@ -13,19 +18,69 @@ public class MoodleClientImpl implements MoodleClient {
 	private static final String GETCOURSES_URI = "/{username}/courses";
 
 	private String uriHostPart;
+	private String username;
+	private String password;
 
 	@Autowired
 	public void setServiceLocation(String serviceLocation) {
 		uriHostPart = serviceLocation;
 	}
 
+	@Autowired
+	public void setUsername(String uname) {
+		username = uname;
+	}
+
+	@Autowired
+	public void setPassword(String passwd) {
+		password = passwd;
+	}
+
+	private HttpEntity<Membership> createMembershipEntity() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		String authorisation = username + ":" + password;
+		System.out.println(authorisation);
+		byte[] encodedAuthorisation = Base64.encodeBase64(authorisation.getBytes());
+		httpHeaders.add("Authorization", "Basic " + new String(encodedAuthorisation));
+		return new HttpEntity<Membership>(httpHeaders);
+	}
+	
+	private HttpEntity<Courses> createCoursesEntity() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		String authorisation = username + ":" + password;
+		byte[] encodedAuthorisation = Base64.encodeBase64(authorisation.getBytes());
+		httpHeaders.add("Authorization", "Basic " + new String(encodedAuthorisation));
+		return new HttpEntity<Courses>(httpHeaders);
+	}
+
+	private String buildRequestUri(String relativeUri) {
+		return uriHostPart + relativeUri;
+	}
+
 	@Override
 	public Membership isMember(String username, String courseid) {
-		return restTemplate.getForObject(uriHostPart + ISMEMBER_URI, Membership.class, username, courseid);
+		ResponseEntity<Membership> response = restTemplate.exchange(
+				buildRequestUri(ISMEMBER_URI),
+				HttpMethod.GET,
+				createMembershipEntity(),
+				Membership.class,
+				username,
+				courseid
+		);
+		
+		return response.getBody();
 	}
 
 	@Override
 	public Courses getCourses(String username) {
-		return restTemplate.getForObject(uriHostPart + GETCOURSES_URI, Courses.class, username);
+		ResponseEntity<Courses> response = restTemplate.exchange(
+				buildRequestUri(GETCOURSES_URI),
+				HttpMethod.GET,
+				createCoursesEntity(),
+				Courses.class,
+				username
+		);
+		
+		return response.getBody();
 	}
 }
