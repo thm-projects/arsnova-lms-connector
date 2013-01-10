e0f5458d80851b465f58bc59abcbdb8383fb796f
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2013-01-10 13:14:45 +0100

    Added some javadoc

--- moodle-connector-client/src/main/java/de/thm/arsnova/connector/moodle/client/MoodleClient.java
+++ moodle-connector-client/src/main/java/de/thm/arsnova/connector/moodle/client/MoodleClient.java
@@ -3,7 +3,47 @@ package de.thm.arsnova.connector.moodle.client;
 import de.thm.arsnova.connector.moodle.model.Courses;
 import de.thm.arsnova.connector.moodle.model.Membership;
 
+/**
+ * <p>A simple client for moodle-connector-service
+ *
+ * <p>This class has to be initialized as spring bean e.g.:
+ *
+ * <pre>
+ * &lt;bean id="moodleClient" scope="singleton" class="de.thm.arsnova.connector.moodle.client.MoodleClientImpl">
+ * 	&lt;property name="serviceLocation" value="http://example.com/" />
+ * 	&lt;property name="username" value="test" />
+ * 	&lt;property name="password" value="test" />
+ * &lt;/bean>
+ * </pre>
+ *
+ * <p>This following example shows the steps to make use of the client library in your project:
+ *
+ * <pre>
+ * &#064;Autowired(required=false)
+ * private MoodleClient moodleClient;
+ *
+ * if (moodleClient != null) {
+ * ... client is initialized ...
+ * }
+ * </pre>
+ *
+ * @author Paul-Christian Volkmer
+ * @since 0.8.1
+ *
+ */
 public interface MoodleClient {
+	/** This service method returns the state of membership
+	 *
+	 * @param username The users name as used in Moodle
+	 * @param courseid The course ID as used in Moodle
+	 * @return The state of membership
+	 */
 	Membership isMember(String username, String courseid);
+
+	/** This service method resturns a list of courses the user is member
+	 *
+	 * @param username The users name as used in Moodle
+	 * @return The list of courses
+	 */
 	Courses getCourses(String username);
 }
