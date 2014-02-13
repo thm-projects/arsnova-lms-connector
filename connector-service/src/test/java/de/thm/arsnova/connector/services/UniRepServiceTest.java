883ea3eaeaf52845c4b57c711a0dcc53a494eb3f
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2014-02-13 15:47:59 +0100

    Added simple service test using mockito

---
+++ connector-service/src/test/java/de/thm/arsnova/connector/services/UniRepServiceTest.java
@@ -0,0 +1,46 @@
+package de.thm.arsnova.connector.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import de.thm.arsnova.connector.core.IliasCategoryNode;
+import de.thm.arsnova.connector.dao.UniRepDao;
+
+public class UniRepServiceTest {
+
+	@InjectMocks
+	private final UniRepService service = new UniRepServiceImpl();
+
+	@Mock
+	private UniRepDao dao;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+
+		when(dao.getTreeObjects(anyInt())).thenReturn(new ArrayList<IliasCategoryNode>());
+	}
+
+	@Test
+	public void testShouldReturnTreeObjects() {
+		assertNotNull(dao.getTreeObjects(42));
+		assertEquals(0, dao.getTreeObjects(42).size());
+	}
+
+	@Test
+	public void testShouldReturnQuestions() {
+		assertNotNull(dao.getQuestion(42));
+		assertEquals(0, dao.getQuestion(42).size());
+	}
+
+}
