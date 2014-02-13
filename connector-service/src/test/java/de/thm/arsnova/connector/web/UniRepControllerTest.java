d218f0bce3843c58c66f449344a4bf54e50d8cf9
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2014-02-13 13:37:04 +0100

    Added tests for UniRepController and moved controller tests

---
+++ connector-service/src/test/java/de/thm/arsnova/connector/web/UniRepControllerTest.java
@@ -0,0 +1,56 @@
+package de.thm.arsnova.connector.web;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import de.thm.arsnova.connector.core.IliasCategoryNode;
+import de.thm.arsnova.connector.core.IliasQuestion;
+import de.thm.arsnova.connector.services.UniRepService;
+
+public class UniRepControllerTest {
+
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private UniRepController controller;
+
+	@Mock
+	private UniRepService service;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = standaloneSetup(controller).build();
+
+		when(service.getTreeObjects(anyInt())).thenReturn(new ArrayList<IliasCategoryNode>());
+		when(service.getQuestions(anyInt())).thenReturn(new ArrayList<IliasQuestion>());
+	}
+
+	@Test
+	public void testShouldReturnTreeData() throws Exception {
+		mockMvc.perform(get("/ilias/123").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+	}
+
+	@Test
+	public void testShouldReturnQuestions() throws Exception {
+		mockMvc.perform(get("/ilias/question/123").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+	}
+}
