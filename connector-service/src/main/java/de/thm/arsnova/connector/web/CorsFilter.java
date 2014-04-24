2ccd4ee1d8af6c76e952bebe943d00efdfe50675
agrt56 <andreas.gaertner@mni.thm.de>
2014-04-25 00:34:28 +0200

    Removed "*" wildcard (Access-Control-Allow-Header) from CorsFiler.

--- connector-service/src/main/java/de/thm/arsnova/connector/web/CorsFilter.java
+++ connector-service/src/main/java/de/thm/arsnova/connector/web/CorsFilter.java
@@ -16,15 +16,14 @@ public class CorsFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {	
-		if (request.getHeader("origin") == null) {
-			response.addHeader("Access-Control-Allow-Origin", "*");
-		} else {
-			response.addHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
-		}
 		response.addHeader("Access-Control-Allow-Credentials", "true");
 		response.addHeader("Access-Control-Allow-Methods", "GET");
 		response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-
+		
+		if (request.getHeader("origin") != null) {
+			response.addHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+		}
+		
 		filterChain.doFilter(request, response);
 	}
 }
