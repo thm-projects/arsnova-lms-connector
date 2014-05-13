7e7ef8b698f94f7fa3c415413974f90ff6dd102f
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2014-05-13 17:02:14 +0200

    Changed PreAuthorize annotations

--- connector-service/src/main/java/de/thm/arsnova/connector/services/ConfigurationService.java
+++ connector-service/src/main/java/de/thm/arsnova/connector/services/ConfigurationService.java
@@ -1,8 +1,14 @@
 package de.thm.arsnova.connector.services;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import de.thm.arsnova.connector.persistence.domain.Configuration;
 
 public interface ConfigurationService {
+
+	@PreAuthorize(value = "hasRole('ADMIN') or hasPermission(#username, 'configuration', 'read')")
 	Configuration getConfigurationElement(String key);
+
+	@PreAuthorize(value = "hasRole('ADMIN') or hasPermission(#username, 'configuration', 'write')")
 	void setConfigurationElement(Configuration config);
 }
--- connector-service/src/main/java/de/thm/arsnova/connector/services/EnabledCategoryService.java
+++ connector-service/src/main/java/de/thm/arsnova/connector/services/EnabledCategoryService.java
@@ -7,10 +7,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import de.thm.arsnova.connector.persistence.domain.EnabledCategory;
 
 public interface EnabledCategoryService {
-	@PreAuthorize(value = "hasRole('ADMIN')")
+	@PreAuthorize(value = "hasRole('ADMIN') or hasPermission(#username, 'enabled_category', 'write')")
 	public void enableCategory(int refId);
 
-	@PreAuthorize(value = "hasRole('ADMIN')")
+	@PreAuthorize(value = "hasRole('ADMIN') or hasPermission(#username, 'enabled_category', 'write')")
 	public void disableCategory(int refId);
 
 	public List<EnabledCategory> getEnabledCategories();
