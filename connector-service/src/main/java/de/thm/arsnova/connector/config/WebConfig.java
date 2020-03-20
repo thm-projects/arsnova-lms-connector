2d17cc2628d96a48cf1f3cd3ce3c0c1b562199c8
Daniel Gerhardt <code@dgerhardt.net>
2020-03-20 13:51:05 +0100

    Setup Jackson's JSR310 (time) module dependency

--- connector-service/src/main/java/de/thm/arsnova/connector/config/WebConfig.java
+++ connector-service/src/main/java/de/thm/arsnova/connector/config/WebConfig.java
@@ -1,11 +1,25 @@
 package de.thm.arsnova.connector.config;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.List;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"de.thm.arsnova.connector.web" })
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.modules(new JavaTimeModule());
+        builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+    }
 }
